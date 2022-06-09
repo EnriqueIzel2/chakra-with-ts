042788958e4d6eab4a2fc6800eaf4e2fcd444cae
EnriqueIzel2 <eleaoizel@gmail.com>
2022-06-08 21:00:39 -0400

    refactor: add components

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,16 @@
 import React from 'react';
-import { ChakraProvider, Container, Flex, VStack } from '@chakra-ui/react'
+import { ChakraProvider, Container, Flex } from '@chakra-ui/react';
+
+import Details from './sections/Details';
+import Cart from './sections/Cart';
 
 function App() {
   return (
     <ChakraProvider>
       <Container maxWidth="container.xl" padding={0}>
         <Flex h="100vh" py={20}>
-          <VStack
-            w="full"
-            h="full"
-            p={10}
-            spacing={10}
-            alignItems="flex-start"
-          >
-            <h1>Hello</h1>
-          </VStack>
-          <VStack
-            w="full"
-            h="full"
-            p={10}
-            spacing={10}
-            alignItems="flex-start"
-            bg="gray.50"
-          >
-            <h1>Hello</h1>
-          </VStack>
+          <Details />
+          <Cart />
         </Flex>
       </Container>
     </ChakraProvider>
