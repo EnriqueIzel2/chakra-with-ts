4bd267d21dc84435b8253c1f3ca01b1623d60155
EnriqueIzel2 <eleaoizel@gmail.com>
2022-06-10 20:01:59 -0400

    feat: add props for responsivity

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,10 @@ function App() {
   return (
     <ChakraProvider>
       <Container maxWidth="container.xl" padding={0}>
-        <Flex h="100vh" py={20}>
+        <Flex
+          h={{ base: "auto", md:"100vh" }}
+          py={[0, 10, 20]}
+          direction={{ base: 'column-reverse', md: "row"}} >
           <Details />
           <Cart />
         </Flex>
